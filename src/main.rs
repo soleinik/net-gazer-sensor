350d15ea2d5a0a4abfa506f1a7b8946ba765aa02
soleinik <162591+soleinik@users.noreply.github.com>
2020-01-23 00:43:42 -0600

    WIP

--- src/main.rs
+++ src/main.rs
@@ -177,9 +177,9 @@ async fn main() -> std::io::Result<()> {
                                         if let Some(icmp) = IcmpPacket::new(ip4pkt.payload()){
 
                                             let dst = ip4pkt.get_destination();
-                                            // if ip != dst{ //only replies back to us
-                                            //     continue;
-                                            // }
+                                            if ip != dst{ //only replies back to us
+                                                continue;
+                                            }
 
                                             let t = icmp.get_icmp_type();
 
@@ -192,10 +192,10 @@ async fn main() -> std::io::Result<()> {
                                                 };
 
                                             match t{
-                                                IcmpTypes::EchoRequest => {
-                                                    let src = ip4pkt.get_source();
-                                                    info!("ICMP-Request {} -> {} [id:{},seq:{},ttl:{}]", src, dst, pkt_id, pkt_seq, ip4pkt.get_ttl());
-                                                }
+                                                // IcmpTypes::EchoRequest => {
+                                                //     let src = ip4pkt.get_source();
+                                                //     info!("ICMP-Request {} -> {} [id:{},seq:{},ttl:{}]", src, dst, pkt_id, pkt_seq, ip4pkt.get_ttl());
+                                                // }
                                                 IcmpTypes::EchoReply => {
                                                     let src = ip4pkt.get_source();
                                                     data_sender.send(AppData::IcmpReply(AppIcmp{src, dst, pkt_id, pkt_seq, ttl:ip4pkt.get_ttl()})).unwrap();
@@ -208,9 +208,9 @@ async fn main() -> std::io::Result<()> {
                                                     let src = ip4pkt.get_source();
                                                     data_sender.send(AppData::IcmpUnreachable(AppIcmp{src, dst, pkt_id, pkt_seq, ttl:ip4pkt.get_ttl()})).unwrap();
                                                 }
-                                                IcmpTypes::Traceroute => {
-                                                    println!("=============> IcmpTypes::Traceroute <=========================")
-                                                }
+                                                // IcmpTypes::Traceroute => {
+                                                //     println!("=============> IcmpTypes::Traceroute <=========================")
+                                                // }
                                                 _ => {
                                                     println!("icmp type:{:?}", t);
                                                     continue
